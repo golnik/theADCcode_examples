777d246781600689bc6dd8e992156a620319f941
Nikolay Golubev <nik.v.golubev@gmail.com>
2025-04-12 15:03:31 -0700

    headers are updated

--- header_guk7.sh
+++ header_guk7.sh
@@ -1,3 +1,4 @@
+# This is an example. Content of this file could vary.
+# But it must specify the command gamess_bin that runs guk v7.x on your system.
 module add guk/7.0
-
 gamess_bin=gamess
--- header_theADCcode.sh
+++ header_theADCcode.sh
@@ -1,6 +1,7 @@
+# This is an example. Content of this file could vary.
+# But it must specify the command theADCcode_bin that runs theADCcode on your system.
 module add gcc/12.3.0
 module add mkl/2024.0
 module add boost/1.82.0/gcc8.5.0
 module add hdf5/intel2024.0.2
-
 theADCcode_bin=$HOME/programs/ADC/theADCcode/build/theADCcode
