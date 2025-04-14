13269cf15c6c9ea3b0a4f2e74cb0728a8e999f1b
Nikolay Golubev <nik.v.golubev@gmail.com>
2025-04-14 12:39:32 -0700

    FT propagator header is added

---
+++ header_FTprop.sh
@@ -0,0 +1,3 @@
+# This is an example. Content of this file could vary.
+# But it must specify the command FTprop_bin that runs FileToolsPropagator on your system.
+FTprop_bin="/software/apps/theADCcode/FileToolsPropagator_v1.0_static_gcc6.3.0.exe"
